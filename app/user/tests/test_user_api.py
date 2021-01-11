c1edc97cef18aefe0d2ee5f5409e283fc0cf4c3e
Pavel <bamboo2panda@gmail.com>
2021-01-11 10:18:59 +0300

    Added recipe model

--- app/user/tests/test_user_api.py
+++ app/user/tests/test_user_api.py
@@ -49,7 +49,7 @@ class PublicUserApiTests(TestCase):
 
         self.assertEqual(res.status_code, status.HTTP_400_BAD_REQUEST)
 
-    def password_too_short(self):
+    def test_password_too_short(self):
         """Test the password must be more than 5 characters"""
         payload = {'email': 'test@example.com',
                    'password': 'pw',
@@ -58,7 +58,7 @@ class PublicUserApiTests(TestCase):
 
         res = self.client.post(CREATE_USER_URL, payload)
 
-        self.assertEqual(res.staus_code, status.HTTP_400_BAD_REQUEST)
+        self.assertEqual(res.status_code, status.HTTP_400_BAD_REQUEST)
         user_exists = get_user_model().objects.filter(
             email=payload['email']
         ).exists()
